8be34621b1c36a237335946f1fd99109591cb0ad
FatemaMajid <fatemamajid2002@gmail.com>
2024-10-24 00:29:40 +0300

    Commit message for additional changes

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ let getStudentsByStage = (stage) => {
 
 let addStudent = (newStudent) => {
   //4
-  students[students.length] = newStudent;
-  return students;
+  return (students[students.length] = newStudent);
 };
 //  addStudent({ id: 21, name: "Alicon Tom" , stage: "Grade 10" , age: 16 }); 
 // console.log(getAllStudents());
@@ -38,26 +37,29 @@ let addStudent = (newStudent) => {
 
 let removeStudentById = (id) => {
   //5
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].id == id) {
-      students.splice(i, 1);
-      break;
-    }
-  }
-  return students;
+  // for (let i = 0; i < students.length; i++) {
+  //   if (students[i].id == id) {
+  //     students.splice(i, 1);
+  //     break;
+  //   }
+  // }
+  // return students;
+  return students.filter((student) => student.id !== id);
 };
 // removeStudentById(19); 
 // console.log(getAllStudents());
 
 let updateStudentById = (id, updatedData) => {
   //6
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].id == id) {
-      students[i] = { ...students[i], ...updatedData };
-      break;
-    }
-  }
-  return students;
+    students = students.map(student => {
+      if (student.id === id) {
+        student.name = updatedData.name;
+        student.age = updatedData.age;
+        student.stage = updatedData.stage;
+      }
+      return student;
+    });
+    return students;
 };
 // updateStudentById(19, { name: "Ava King" , stage: "Grade 10" , age: 15 });
 // console.log(getAllStudents());
@@ -94,33 +96,35 @@ let getStudentByName = (name) => {
 
 let sortStudentsByName = () => {
   //11
-  var temp;
-  for (let i = 0; i < students.length - 1; i++) {
-    for (let j = 0; j < students.length - 1 - i; j++) {
-      if (students[j].name > students[j + 1].name) {
-        temp = students[j];
-        students[j] = students[j + 1];
-        students[j + 1] = temp;
-      }
-    }
-  }
-  return students;
+  // var temp;
+  // for (let i = 0; i < students.length - 1; i++) {
+  //   for (let j = 0; j < students.length - 1 - i; j++) {
+  //     if (students[j].name > students[j + 1].name) {
+  //       temp = students[j];
+  //       students[j] = students[j + 1];
+  //       students[j + 1] = temp;
+  //     }
+  //   }
+  // }
+  // return students;
+  return students.sort((a, b) => a.name.localeCompare(b.name));
 };
 // console.log(sortStudentsByName());
 
 let sortStudentsByAge = () => {
   //12
-  var temp;
-  for (let i = 0; i < students.length - 1; i++) {
-    for (let j = 0; j < students.length - 1 - i; j++) {
-      if (students[j].age > students[j + 1].age) {
-        temp = students[j];
-        students[j] = students[j + 1];
-        students[j + 1] = temp;
-      }
-    }
-  }
-  return students;
+  // var temp;
+  // for (let i = 0; i < students.length - 1; i++) {
+  //   for (let j = 0; j < students.length - 1 - i; j++) {
+  //     if (students[j].age > students[j + 1].age) {
+  //       temp = students[j];
+  //       students[j] = students[j + 1];
+  //       students[j + 1] = temp;
+  //     }
+  //   }
+  // }
+  // return students;
+  return students.sort((a, b) => a.age - b.age);
 };
 // console.log(sortStudentsByAge());
 
@@ -150,13 +154,19 @@ let countStudentsByStage = (stage) => {
 
 let incrementStudentAgeById = (id) => {
   //17
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].id == id) {
-      students[i].age++;
-      break;
+  // for (let i = 0; i < students.length; i++) {
+  //   if (students[i].id == id) {
+  //     students[i].age++;
+  //     break;
+  //   }
+  // }
+  // return students;
+  return students.map((student) => {
+    if (student.id === id) {
+      return { ...student, age: student.age + 1 };
     }
-  }
-  return students;
+    return student;
+  });
 };
 // console.log(getStudentById(19));
 // console.log(incrementStudentAgeById(19));
@@ -164,13 +174,19 @@ let incrementStudentAgeById = (id) => {
 
 let decrementStudentAgeById = (id) => {
   //18
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].id == id) {
-      students[i].age--;
-      break;
-    }
-  }
-  return students;
+  // for (let i = 0; i < students.length; i++) {
+  //   if (students[i].id == id) {
+  //     students[i].age--;
+  //     break;
+  //   }
+  // }
+  // return students;
+  return students.map((student) => {
+    if (student.id === id) {
+       return { ...student, age: student.age - 1 };
+     }
+     return student;
+   });
 };
 // console.log(getStudentById(3));
 // console.log(decrementStudentAgeById(3));
@@ -243,128 +259,138 @@ let getOldestStudent = () => {
 
 let hasStudentWithName = (name) => {
   //26
-  var fullName, firstName;
-  for (let i = 0; i < students.length; i++) {
-    fullName = students[i].name
-    firstName = fullName.split(" ")[0];
-    if (firstName === name || fullName === name) {
-      return true;
-    }
-  }
-  return false;
+  // var fullName, firstName;
+  // for (let i = 0; i < students.length; i++) {
+  //   fullName = students[i].name
+  //   firstName = fullName.split(" ")[0];
+  //   if (firstName === name || fullName === name) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+  return students.some((student) => student.name.includes(name));
 };
 // console.log(hasStudentWithName("Sophi"));
 
 let getNamesOfAllStudents = () => {
   //27
-  var names = [];
-  for (let i = 0; i < students.length; i++) {
-    names.push(students[i].name);
-  }
-  return names;
+  // var names = [];
+  // for (let i = 0; i < students.length; i++) {
+  //   names.push(students[i].name);
+  // }
+  // return names;
+  return students.map((student) => student.name);
 };
 // console.log(getNamesOfAllStudents());
 
 
 let getAllStudentIds = () => {
   //28
-  var ids = [];
-  for (let i = 0; i < students.length; i++) {
-    ids.push(students[i].id);
-  }
-  return ids;
+  // var ids = [];
+  // for (let i = 0; i < students.length; i++) {
+  //   ids.push(students[i].id);
+  // }
+  // return ids;
+  return students.map((student) => student.id);
 };
 // console.log(getAllStudentIds());
 
 let getAllStudentStages = () => {
   //29
-  var stages = [];
-  for (let i = 0; i < students.length; i++) {
-    if (!stages.includes(students[i].stage)) {
-      stages.push(students[i].stage);
-    }
-  }
-  return stages;
+  // var stages = [];
+  // for (let i = 0; i < students.length; i++) {
+  //   if (!stages.includes(students[i].stage)) {
+  //     stages.push(students[i].stage);
+  //   }
+  // }
+  // return stages;
+  return students.map((student) => student.stage).filter((stage, index, array) => array.indexOf(stage) === index);
 };
 // console.log(getAllStudentStages());
 
 let getStudentsWithNamesStartingWith = (letter) => {
   //30
-  var names = [];
-  letter = letter.toUpperCase();
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].name[0] === letter) {
-      names.push(students[i]);
-    }
-  }
-  return names;
+  // var names = [];
+  // letter = letter.toUpperCase();
+  // for (let i = 0; i < students.length; i++) {
+  //   if (students[i].name[0] === letter) {
+  //     names.push(students[i]);
+  //   }
+  // }
+  // return names;
+  return students.filter((student) => student.name[0] === letter.toUpperCase());
 };
 // console.log(getStudentsWithNamesStartingWith("j"));
 
 
 let getStudentsWithNamesEndingWith = (letter) => {
   //31
-  var names = [];
-  letter = letter.toLowerCase();
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].name[students[i].name.length - 1] === letter) {
-      names.push(students[i]);
-    }
-  }
-  return names;
+  // var names = [];
+  // letter = letter.toLowerCase();
+  // for (let i = 0; i < students.length; i++) {
+  //   if (students[i].name[students[i].name.length - 1] === letter) {
+  //     names.push(students[i]);
+  //   }
+  // }
+  // return names;
+  return students.filter((student) => student.name[student.name.length - 1] === letter.toLowerCase());
 };
 // console.log(getStudentsWithNamesEndingWith("n"));
 
 let getStudentsWithNameLengthGreaterThan = (length) => {
   //32
-  var names = [];
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].name.length > length) {
-      names.push(students[i]);
-    }
-  }
-  return names;
+  // var names = [];
+  // for (let i = 0; i < students.length; i++) {
+  //   if (students[i].name.length > length) {
+  //     names.push(students[i]);
+  //   }
+  // }
+  // return names;
+  return students.filter((student) => student.name.length > length);
 };
 // console.log(getStudentsWithNameLengthGreaterThan(6));
 
 let getStudentsWithNameLengthLessThan = (length) => {
   //33
-  var names = [];
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].name.length < length) {
-      names.push(students[i]);
-    }
-  }
-  return names;
+  // var names = [];
+  // for (let i = 0; i < students.length; i++) {
+  //   if (students[i].name.length < length) {
+  //     names.push(students[i]);
+  //   }
+  // }
+  // return names;
+  return students.filter((student) => student.name.length < length);
 };
 // console.log(getStudentsWithNameLengthLessThan(6));
 
 let getAllStudentsSortedById = () => {
   //34
-  var temp;
-  for (let i = 0; i < students.length - 1; i++) {
-    for (let j = 0; j < students.length - 1 - i; j++) {
-      if (students[j].id > students[j + 1].id) {
-        temp = students[j];
-        students[j] = students[j + 1];
-        students[j + 1] = temp;
-      }
-    }
-  }
+  // var temp;
+  // for (let i = 0; i < students.length - 1; i++) {
+  //   for (let j = 0; j < students.length - 1 - i; j++) {
+  //     if (students[j].id > students[j + 1].id) {
+  //       temp = students[j];
+  //       students[j] = students[j + 1];
+  //       students[j + 1] = temp;
+  //     }
+  //   }
+  // }
   
-  return students;
+  // return students;
+  return students.sort((a, b) => a.id - b.id);
 };
 // console.log(getAllStudentsSortedById());
 
 let reverseStudentList = () => {
   //35
-  var temp;
-  for (let i = 0; i < students.length / 2; i++) {
-    temp = students[i];
-    students[i] = students[students.length - 1 - i];
-    students[students.length - 1 - i] = temp;
-  }
-  return students;
+  // var temp;
+  // for (let i = 0; i < students.length / 2; i++) {
+  //   temp = students[i];
+  //   students[i] = students[students.length - 1 - i];
+  //   students[students.length - 1 - i] = temp;
+  // }
+  // return students;
+  return students.reverse();
 };
 // console.log(reverseStudentList());
 
